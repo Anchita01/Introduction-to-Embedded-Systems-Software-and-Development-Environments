42ce9907b6d75608ac023fcb5ff527d4fe016960
Anchita01 <gunjagoyal14@gmail.com>
2023-10-02 03:51:17 -0600

    Adding code outline and documentation for C1M1 Assessment

--- Week1/stats.c
+++ Week1/stats.c
@@ -9,23 +9,31 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
  *
- * <Add Extended Description Here>
+ * @brief Coursera Course 1 Module 1 Assessment 1 (C1M1)
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * Inclusion of the main program and all function definitions
+ * requird for the C1M1 assessment.
+ *
+ * @author Anchita Goyal
+ * @date 30/09/2023 >
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
+
+/*static function to swap two unsigned int values*/
+static void swap32(unsigned char* a, unsigned char* b)
+{
+	unsigned char temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
 void main() {
 
@@ -36,8 +44,97 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+
   /* Statistics and Printing Functions Go Here */
+	print_statistics(test,SIZE);
 
 }
 
+/*function to print the statistics of an array*/
+void print_statistics(unsigned char* arr, unsigned int size)
+{
+	printf("\nUnsorted Array:\n");
+	print_array(arr,size);
+
+	printf("\n\nMedian of the given array = %d\n",find_median(arr, size));
+
+	printf("\nMean of the given array = %f\n",find_mean(arr, size));
+
+	printf("\nMaximum value in the given array = %d\n", find_maximum(arr, size));
+
+	printf("\nMinimum value in the given array = %d\n", find_minimum(arr, size));
+
+	sort_array(arr,size);
+	printf("\nSorted Array:\n");
+	print_array(arr,size);
+}
+
+/*function to print array elements with positions*/
+void print_array(unsigned char* arr, unsigned int size)
+{
+	for(unsigned char i=0; i<size; i++)
+	{
+		printf("arr[%i]=%i,\t",i,arr[i]);
+	}
+}
+
+/*function to find the median of an array*/
+unsigned char find_median(unsigned char* arr, unsigned int size)
+{
+	unsigned char median = 0, rem = 0;
+	sort_array(arr, size);
+	rem = size%2;
+	if(rem != 0)	//odd number of elements in array
+	{
+		median = arr[size/2];
+	}
+	else		//Even number of elements in array
+	{
+		median = ( arr[size/2] + arr[(size/2) - 1])/2;
+	}	
+	return median;
+} 
+
+/*function to find the mean of an array*/
+float find_mean (unsigned char* arr, unsigned int size){
+  float mean = 0;
+  for (int i=0; i<size; i++){
+    mean = mean + arr[i] /* *(array + i) */;
+  }
+  mean = mean / ((float) size); //must type cast one of the two integers to float for accurate calculation.
+  return mean;
+}	
+
+/*function to return maximum value of an array*/
+unsigned char find_maximum(unsigned char* arr, unsigned int size)
+{
+	unsigned char max = 0;
+	sort_array(arr, size);
+	max = arr[0];
+	return max;
+}	
+
+/*function to return minimum value of an array*/
+unsigned char find_minimum(unsigned char* arr, unsigned int size)
+{
+	unsigned char min = 0;
+	sort_array(arr, size);
+	min = arr[size-1];
+	return min;
+}	
+
+/*function to sort an array in descending order*/
+void sort_array(unsigned char* arr, unsigned int size)
+{
+	for(unsigned char i=0; i<size; i++)
+	{
+		for(unsigned char j = i+1; j<size; j++)
+		{
+			if(arr[i]<arr[j])	//comparing the array elements.
+			{
+				swap32(&arr[i], &arr[j]);	//swap the array elements.
+			}
+		}
+	}
+}	
 /* Add other Implementation File Code Here */
